e97bd119521504fd3f490d43241d6f91eaf204f0
Semih Turan <semihturan@outlook.com>
2016-05-16 09:23:15 +0300

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,265 +6,274 @@ module.exports = function(grunt) {
     // Load all Grunt tasks
     require('jit-grunt')(grunt);
 
+    grunt.loadNpmTasks('grunt-build-control');
+    grunt.loadNpmTasks('grunt-gh-pages');
+
     grunt.initConfig({
-        app: {
-            app: '',
-            dist: 'dist',
-            baseurl: ''
+      'gh-pages': {
+        options: {
+          base: 'dist'
         },
-        watch: {
-            jekyll: {
-                files: [
-                    '_site/**/*.{html,yml,md,mkd,markdown}'                ],
-                tasks: ['jekyll:server']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '_site/**/*.{html,yml,md,mkd,markdown}',
-                    '_site/css/*.css',
-                    '_site/js/*.js',
-                    '_site/images/*.{svg,gif,jpg,jpeg,png,svg,webp}'
-                ]
-            }
-        },
-        connect: {
-            options: {
-                port: 4000,
-                livereload: 35729,
-                // change this to '0.0.0.0' to access the server from outside
-                hostname: '0.0.0.0'
-            },
-            livereload: {
-                options: {
-                    open: {
-                        target: 'http://localhost:4000/<%= app.baseurl %>'
-                    },
-                    base: [
-                        '_site',
-                        '.tmp',
-                        '<%= app.app %>'
-                    ]
-                }
-            },
-            dist: {
-                options: {
-                    open: {
-                        target: 'http://localhost:4000/<%= app.baseurl %>'
-                    },
-                    base: [
-                        '<%= app.dist %>',
-                        '.tmp'
-                    ]
-                }
-            }
-        },
-        clean: {
-            server: [
-                '_site',
-                '.tmp'
-            ],
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        '<%= app.dist %>/*',
-                        '!<%= app.dist %>/.git*'
-                    ]
-                }]
-            }
-        },
-        jekyll: {
-            options: {
-                config: '_config.yml',
-                src: '<%= app.app %>'
-            },
-            dist: {
-                options: {
-                    dest: '<%= app.dist %>/<%= app.baseurl %>',
-                }
-            },
-            server: {
-                options: {
-                    config: '_config.yml',
-                    dest: '_site/<%= app.baseurl %>'
-                }
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    removeEmptyAttributes: true,
-                    minifyJS: true,
-                    minifyCSS: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= app.dist %>/<%= app.baseurl %>',
-                    src: '**/*.html',
-                    dest: '<%= app.dist %>/<%= app.baseurl %>'
-                }]
-            },
-            server: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    removeEmptyAttributes: true,
-                    minifyJS: true,
-                    minifyCSS: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: '_site/',
-                    src: '*.html',
-                    dest: '_site/'
-                }]
-            }
-        },
-        sass: {
-            server: {
-                options: {
-                    sourceMap: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: '/css',
-                    src: '*.{scss,sass}',
-                    dest: '_site/css',
-                    ext: '.css'
-                }]
-            },
-            dist: {
-                options: {
-                    outputStyle: 'compressed'
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= app.app %>/sass',
-                    src: '**/*.{scss,sass}',
-                    dest: '<%= app.dist %>/<%= app.baseurl %>/css',
-                    ext: '.css'
-                }]
-            }
-        },
-        autoprefixer: {
-            options: {
-                browsers: ['last 3 versions']
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '_site/css',
-                    src: '*.css',
-                    dest: '_site/css'
-                }]
-            }
-        },
-        critical: {
-            dist: {
-                options: {
-                    base: './',
-                    css: [
-                        '.tmp/<%= app.baseurl %>/css/build.css'
-                    ],
-                    minify: true,
-                    width: 320,
-                    height: 480
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= app.dist %>/<%= app.baseurl %>',
-                    src: ['**/*.html'],
-                    dest: '<%= app.dist %>/<%= app.baseurl %>'
-                }]
-            }
-        },
-        cssmin: {
-            dist: {
-                options: {
-                    keepSpecialComments: 0,
-                    check: 'gzip'
-                },
-                files: [{
-                    expand: true,
-                    cwd: '_site/css',
-                    src: ['*.css'],
-                    dest: '_site/css'
-                }]
-            },
-            server: {
-                options: {
-                    keepSpecialComments: 0,
-                    check: 'gzip'
-                },
-                files: [{
-                    expand: true,
-                    cwd:  '.tmp/<%= app.baseurl %>/css',
-                    src: ['.css'],
-                    dest: '.tmp/<%= app.baseurl %>/css',
-                }]
-            }
-        },
-        imagemin: {
-            options: {
-                progressive: true
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= app.dist %>/<%= app.baseurl %>/images',
-                    src: '**/*.{jpg,jpeg,png,gif}',
-                    dest: '<%= app.dist %>/<%= app.baseurl %>/images'
-                }]
-            }
-        },
-        svgmin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= app.dist %>/<%= app.baseurl %>/images',
-                    src: '**/*.svg',
-                    dest: '<%= app.dist %>/<%= app.baseurl %>/images'
-                }]
-            }
-        },
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '.tmp/<%= app.baseurl %>',
-                    src: [
-                        'css/**/*',
-                        'js/**/*'
-                    ],
-                    dest: '<%= app.dist %>/<%= app.baseurl %>'
-                }]
-            }
-        },
-        buildcontrol: {
-            dist: {
-                options: {
-                    dir: '<%= app.dist %>/<%= app.baseurl %>',
-                    remote: 'semihturan@github.com:semihturan/valensas.com-demo.git',
-                    branch: 'gh-pages',
-                    commit: true,
-                    push: true,
-                    connectCommits: false
-                }
-            }
+        src: ['**']
+      },
+      app: {
+        app: '',
+        dist: 'dist',
+        baseurl: ''
+      },
+      watch: {
+          jekyll: {
+              files: [
+                  '_site/**/*.{html,yml,md,mkd,markdown}'                ],
+              tasks: ['jekyll:server']
+          },
+          livereload: {
+              options: {
+                  livereload: '<%= connect.options.livereload %>'
+              },
+              files: [
+                  '_site/**/*.{html,yml,md,mkd,markdown}',
+                  '_site/css/*.css',
+                  '_site/js/*.js',
+                  '_site/images/*.{svg,gif,jpg,jpeg,png,svg,webp}'
+              ]
+          }
+      },
+      connect: {
+          options: {
+              port: 4000,
+              livereload: 35729,
+              // change this to '0.0.0.0' to access the server from outside
+              hostname: '0.0.0.0'
+          },
+          livereload: {
+              options: {
+                  open: {
+                      target: 'http://localhost:4000/<%= app.baseurl %>'
+                  },
+                  base: [
+                      '_site',
+                      '.tmp',
+                      '<%= app.app %>'
+                  ]
+              }
+          },
+          dist: {
+              options: {
+                  open: {
+                      target: 'http://localhost:4000/<%= app.baseurl %>'
+                  },
+                  base: [
+                      '<%= app.dist %>',
+                      '.tmp'
+                  ]
+              }
+          }
+      },
+      clean: {
+          server: [
+              '_site',
+              '.tmp'
+          ],
+          dist: {
+              files: [{
+                  dot: true,
+                  src: [
+                      '.tmp',
+                      '<%= app.dist %>/*',
+                      '!<%= app.dist %>/.git*'
+                  ]
+              }]
+          }
+      },
+      jekyll: {
+          options: {
+              config: '_config.yml',
+              src: '<%= app.app %>'
+          },
+          dist: {
+              options: {
+                  dest: '<%= app.dist %>',
+              }
+          },
+          server: {
+              options: {
+                  config: '_config.yml',
+                  dest: '_site/<%= app.baseurl %>'
+              }
+          }
+      },
+      htmlmin: {
+          dist: {
+              options: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  collapseBooleanAttributes: true,
+                  removeAttributeQuotes: true,
+                  removeRedundantAttributes: true,
+                  removeEmptyAttributes: true,
+                  minifyJS: true,
+                  minifyCSS: true
+              },
+              files: [{
+                  expand: true,
+                  cwd: '<%= app.dist %>',
+                  src: '**/*.html',
+                  dest: '<%= app.dist %>'
+              }]
+          },
+          server: {
+              options: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  collapseBooleanAttributes: true,
+                  removeAttributeQuotes: true,
+                  removeRedundantAttributes: true,
+                  removeEmptyAttributes: true,
+                  minifyJS: true,
+                  minifyCSS: true
+              },
+              files: [{
+                  expand: true,
+                  cwd: '_site/',
+                  src: '*.html',
+                  dest: '_site/'
+              }]
+          }
+      },
+      sass: {
+          server: {
+              options: {
+                  sourceMap: true
+              },
+              files: [{
+                  expand: true,
+                  cwd: '/css',
+                  src: '*.{scss,sass}',
+                  dest: '_site/css',
+                  ext: '.css'
+              }]
+          },
+          dist: {
+              options: {
+                  outputStyle: 'compressed'
+              },
+              files: [{
+                  expand: true,
+                  cwd: '<%= app.app %>/sass',
+                  src: '**/*.{scss,sass}',
+                  dest: '<%= app.dist %>/css',
+                  ext: '.css'
+              }]
+          }
+      },
+      autoprefixer: {
+          options: {
+              browsers: ['last 3 versions']
+          },
+          dist: {
+              files: [{
+                  expand: true,
+                  cwd: '<%= app.dist %>/css',
+                  src: '*.css',
+                  dest: '<%= app.dist %>/css'
+              }]
+          }
+      },
+      critical: {
+          dist: {
+              options: {
+                  base: './',
+                  css: [
+                      '.tmp/css/build.css'
+                  ],
+                  minify: true,
+                  width: 320,
+                  height: 480
+              },
+              files: [{
+                  expand: true,
+                  cwd: '<%= app.dist %>',
+                  src: ['**/*.html'],
+                  dest: '<%= app.dist %>'
+              }]
+          }
+      },
+      cssmin: {
+          dist: {
+              options: {
+                  keepSpecialComments: 0,
+                  check: 'gzip'
+              },
+              files: [{
+                  expand: true,
+                  cwd: '_site/css',
+                  src: ['*.css'],
+                  dest: '_site/css'
+              }]
+          },
+          server: {
+              options: {
+                  keepSpecialComments: 0,
+                  check: 'gzip'
+              },
+              files: [{
+                  expand: true,
+                  cwd:  '.tmp/<%= app.baseurl %>/css',
+                  src: ['.css'],
+                  dest: '.tmp/<%= app.baseurl %>/css',
+              }]
+          }
+      },
+      imagemin: {
+          options: {
+              progressive: true
+          },
+          dist: {
+              files: [{
+                  expand: true,
+                  cwd: '<%= app.dist %>/images',
+                  src: '*.{jpg,jpeg,png,gif}',
+                  dest: '<%= app.dist %>/images'
+              }]
+          }
+      },
+      svgmin: {
+          dist: {
+              files: [{
+                  expand: true,
+                  cwd: '<%= app.dist %>/images',
+                  src: '*.svg',
+                  dest: '<%= app.dist %>/images'
+              }]
+          }
+      },
+      copy: {
+          dist: {
+              files: [{
+                  expand: true,
+                  dot: true,
+                  cwd: '.tmp',
+                  src: [
+                      'css/*',
+                      'js/*'
+                  ],
+                  dest: '<%= app.dist %>'
+              }]
+          }
+      },
+      buildcontrol: {
+        dist: {
+          options: {
+            dir: '<%= app.dist %>',
+            remote: 'semihturan@github.com:semihturan/valensas.com-demo.git',
+            branch: 'gh-pages',
+            commit: true,
+            push: true,
+            connectCommits: false
+          }
         }
+      }
     });
 
     // Define Tasks
@@ -280,7 +289,6 @@ module.exports = function(grunt) {
             'autoprefixer',
             'cssmin:server',
             'htmlmin:server',
-            // 'uglify',
             'connect:livereload',
             'watch'
         ]);
@@ -295,13 +303,12 @@ module.exports = function(grunt) {
         'clean:dist',
         'jekyll:dist',
         // 'imagemin',
-        // 'svgmin',
+        'svgmin',
         'sass:dist',
         'autoprefixer',
-        'cssmin',
-        // 'uglify',
-        'critical',
-        'htmlmin'
+        'cssmin:dist',
+        // 'critical',
+        // 'htmlmin'
     ]);
 
     grunt.registerTask('deploy', [
